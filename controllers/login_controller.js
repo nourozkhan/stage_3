1b5257871a067307a6c9d2bcff8df2baba52860d
nourozkhan <nouroz.khan1.nk@gmail.com>
2017-09-07 16:05:36 +0500

    Update login_controller.js

--- controllers/login_controller.js
+++ controllers/login_controller.js
@@ -2,7 +2,7 @@ const Login = require('../models/login');
 
 module.exports = {
     get(req, res, next) {
-        Todo.find({})
+        Login.find({})
             .then(todo => res.send(todo))
             .catch(next);
     },
@@ -10,7 +10,7 @@ module.exports = {
         
         const todoProps = req.body;
 
-        Todo.create(todoProps)
+        Login.create(todoProps)
             .then(todo => res.send(todo))
             .catch(next)
     }

3b34ad982cad9721ba5adbd5c2945b79d05f60ad
nourozkhan <nouroz.khan1.nk@gmail.com>
2017-09-08 14:11:36 +0500

    Update patient.js

--- models/patient.js
+++ models/patient.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PatientSchema = new Schema({
+    particularDoctor: {
+        type: String,
+        required: true
+    },
     patient_name: {
         type: String,
         required: true

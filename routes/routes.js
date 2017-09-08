c1844b3ca96b90e82cbc759999cfc6884f1574cb
nourozkhan <nouroz.khan1.nk@gmail.com>
2017-09-08 16:07:46 +0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,6 @@ const LoginController = require('../controllers/login_controller');
 module.exports = (app) => {
     app.get('/api/patient', PatientsController.get );
     app.post('/api/patient', PatientsController.create );
-    app.get('/api/login', LoginController.get );
+    app.post('/api/login', LoginController.get );
     app.post('/api/login', LoginController.create );   
 }
